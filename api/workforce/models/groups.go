50f63492e53f35197bd2a93b25990a3cbfb11994
Jeff Gregory <jeffrey.s.gregory@me.com>
2021-12-08 16:48:22 -0600

    WIP: Updated groups endpoint and added init data

--- api/workforce/models/groups.go
+++ api/workforce/models/groups.go
@@ -10,7 +10,8 @@ import (
 )
 
 type Group struct {
-	OfficeSymbol  string     `json:"office_symbol"`
+	UID           string     `json:"uid"`
+	Symbol        string     `json:"symbol"`
 	Name          string     `json:"name"`
 	Slug          string     `json:"slug"`
 	Last_verified *time.Time `json:"last_verified"`
@@ -20,11 +21,11 @@ type Group struct {
 func ListGroupsByOffice(db *pgxpool.Pool, sym string) ([]*Group, error) {
 	sg := make([]*Group, 0)
 	rows, err := db.Query(context.Background(),
-		`SELECT o1.symbol AS office_symbol, og."name", og.slug, og.last_verified
-		FROM office AS o1
-		JOIN office AS o2 ON o1.parent_id = o2.id
-		JOIN office_group AS og ON o1.id = og.office_id
-		WHERE o1.symbol = $1`,
+		`SELECT concat(lower(f.symbol), '-', g.slug) AS uid,
+			f.symbol, g.name, g.slug, g.last_verified
+		FROM office_group g
+		JOIN office f ON f.id = g.office_id
+		WHERE f.symbol = $1`,
 		strings.ToUpper(sym),
 	)
 	if err != nil {
