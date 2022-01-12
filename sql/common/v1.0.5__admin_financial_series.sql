b22fbe9b3ea68eee3a6fb28b99eee3fca60bf9c2
Jeff Gregory <37386268+jeffsuperglide@users.noreply.github.com>
2022-01-12 15:10:34 -0600

    Update v1.0.5__admin_financial_series.sql
     Missing comma.

--- sql/common/v1.0.5__admin_financial_series.sql
+++ sql/common/v1.0.5__admin_financial_series.sql
@@ -4,7 +4,7 @@ INSERT INTO occupation_code (id, code, name) VALUES
     ('6e340724-7578-4faf-adc6-20428d187692', '0341', 'Administrative Officer'),
     ('501deb3a-1294-4325-9506-5565603a08e3', '0343', 'Management and Program Analysis'),
     ('b5f28408-3579-402e-a3ff-fe8b70d40130', '0344', 'Management and Program Clerical and Assistance'),
-    ('9ba50a20-05e0-4657-bb5c-f716e8d6b1e9', '0501', 'Financial Administration and Program')
+    ('9ba50a20-05e0-4657-bb5c-f716e8d6b1e9', '0501', 'Financial Administration and Program'),
     ('6d2d219e-29f8-45f9-b5de-76cbffa03ee7', '0503', 'Financial Clerical and Technician'),
     ('9a2d67f2-0841-4953-b881-2dca3be0f05b', '0505', 'Financial Management'),
     ('755267f3-e135-40e3-b4a1-d1a14aa461e6', '0560', 'Budget Analysis'),
