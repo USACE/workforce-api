43577d5d4525c89786fd6c3a01bbcfbd9996a903
Jeff Gregory <jeffrey.s.gregory@me.com>
2022-01-12 15:46:50 -0600

    Proposed Postman Tests

--- run_tests.sh
+++ run_tests.sh
@@ -3,4 +3,4 @@ docker run \
     -v $(pwd)/tests:/etc/newman --network=workforce-api_default \
     --entrypoint /bin/bash postman/newman:ubuntu \
     -c "newman run /etc/newman/workforce.postman_collection.json \
-        --environment=/etc/newman/workforce-docker-compose.postman_environment.json"
+        --env-var base_url=http://api"
