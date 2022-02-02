ee471ef41fc01c6208e6b7c6ed4eb4d1fcd95cd2
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2022-02-02 18:28:46 -0500

    add new payplan and two occupations (#47)

---
+++ sql/common/V1.0.13__dk_pay_plan.sql
@@ -0,0 +1,7 @@
+INSERT INTO pay_plan (id, code, name) VALUES
+    ('055f01da-d38d-4722-8e4c-d0f483007a82', 'DK', 'Demonstration General Support');
+
+-- occupation_codes
+INSERT INTO occupation_code (id, code, name) VALUES
+    ('df7970b4-2d71-4178-ad47-c90dc1f6aa87', '0871', 'Naval Architecture'),
+    ('cdae0082-0bd2-495b-a0d2-40facf8089af', '1310', 'Physics');
