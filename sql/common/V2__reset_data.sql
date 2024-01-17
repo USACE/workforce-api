beb4addc780f1de751460c84858db01d5575a3e5
Jeff Gregory <jeffrey.s.gregory@me.com>
2024-01-17 11:41:11 -0600

    App Data Reset
    Reset data migration added to common dir.  Data downloaded and set
    in migration files in local dir.

---
+++ sql/common/V2__reset_data.sql
@@ -0,0 +1,13 @@
+-- data reset
+
+-- remove all rows from position
+DELETE FROM position;
+
+-- remove all rows from occupancy
+DELETE FROM occupancy;
+
+-- remove all rows from occupant_credentials
+DELETE FROM occupant_credentials;
+
+-- remove all rows from occupant_expertise
+DELETE FROM occupant_expertise;
